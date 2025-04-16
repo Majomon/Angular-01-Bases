ee6f07792d66ef61c97f8916a5ca793957470752
Mauri <mauri.monzon91@gmail.com>
2025-04-16 20:12:24 -0300

    Preparando la pagina

---
+++ src/app/pages/dragonball/dragonball-ball.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  templateUrl: './dragonball-ball.component.html',
+  styles: `
+  button{
+    padding: 5px;
+    margin: 5px 10px;
+    width:75px;
+  }
+  `,
+})
+export class DragonBallComponent {}
