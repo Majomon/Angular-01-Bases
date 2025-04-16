5b2dde4005e540158a2d47f59d3f67e008a83249
Mauri <mauri.monzon91@gmail.com>
2025-04-16 20:29:45 -0300

    ng class

--- src/app/pages/dragonball/dragonball-ball.component.ts
+++ src/app/pages/dragonball/dragonball-ball.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 interface Character {
   id: number;
@@ -8,18 +9,20 @@ interface Character {
 
 @Component({
   templateUrl: './dragonball-ball.component.html',
-  styles: `
-  button{
-    padding: 5px;
-    margin: 5px 10px;
-    width:75px;
-  }
-  `,
+  imports: [
+    //NgClass
+  ],
 })
 export class DragonBallComponent {
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8500 },
+    { id: 2, name: 'Vegeta', power: 9000 },
     { id: 3, name: 'Piccolo', power: 6000 },
   ]);
+
+  /*   powerClasses = computed(() => {
+    return {
+      'text-danger': true,
+    };
+  }); */
 }
