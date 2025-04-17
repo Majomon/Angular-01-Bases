c2fd990eab592742f2113571970851db87afadb4
Mauri <mauri.monzon91@gmail.com>
2025-04-16 21:04:32 -0300

    agregar personaje

--- src/app/pages/dragonball/dragonball-ball.component.ts
+++ src/app/pages/dragonball/dragonball-ball.component.ts
@@ -14,6 +14,9 @@ interface Character {
   ],
 })
 export class DragonBallComponent {
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 9000 },
@@ -26,4 +29,22 @@ export class DragonBallComponent {
       'text-danger': true,
     };
   }); */
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) return;
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
